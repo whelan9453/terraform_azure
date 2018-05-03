2c8dc0e28bb820fa9b548b00edf059ac156571ac
whelan957 <sitdtest@gmail.com>
2018-05-03 12:17:59 +0800

    Delete OS disk on VM termination.

--- openvpn/client/open_vpn_client.tf
+++ openvpn/client/open_vpn_client.tf
@@ -164,8 +164,7 @@ resource "azurerm_virtual_machine" "vm" {
   vm_size               = "Standard_DS1_v2"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
-  # delete_os_disk_on_termination = true
-
+  delete_os_disk_on_termination = true
 
   # Uncomment this line to delete the data disks automatically when deleting the VM
   # delete_data_disks_on_termination = true
